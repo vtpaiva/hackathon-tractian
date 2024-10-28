95e513c76bd9ebe68ae5fe54e2ae74bddd209fef
vtpaiva <vitor_brito@usp.br>
2024-10-28 14:55:43 -0300

    Adição de código SAP das ferramentas.

--- meu_script.py
+++ meu_script.py
@@ -1,6 +1,8 @@
 import sys
 from openai import OpenAI
 import openai
+import pandas as pd
+import json
 client = OpenAI()
 
 def process_audio(file_path):
@@ -13,6 +15,16 @@ def process_audio(file_path):
     texto = transcription.text
     return texto
 
+def formatar_dados_csv(caminho_csv):
+    # Ler o CSV usando pandas
+    df = pd.read_csv(caminho_csv)
+    
+    # Converter para uma string legível
+    dados_json = df.to_json(orient='records')
+    dados_json_texto = json.dumps(json.loads(dados_json), indent=2)
+
+    return dados_json_texto
+
 def gerar_resposta(prompt):
     response = openai.chat.completions.create(
         model="gpt-4o",  # Escolha o modelo, como "gpt-3.5-turbo" ou "gpt-4"
@@ -20,7 +32,6 @@ def gerar_resposta(prompt):
             {"role": "system", "content": "Você é um assistente que ajuda com perguntas em Python."},
             {"role": "user", "content": prompt}
         ],
-        max_tokens=100,  # Limite de tokens na resposta
         n=1,  # Número de respostas
         stop=None,  # Pode definir uma palavra de parada, se necessário
         temperature=0.7  # Controla a criatividade da resposta (entre 0 e 1)
@@ -35,12 +46,13 @@ if __name__ == "__main__":
 
     file_path = sys.argv[1]
     resultado = process_audio(file_path)
-    print(resultado)
-
-    print('------------------RESPOSTAGPT------------------------')
 
     # Exemplo de uso da função
-    prompt = f'A partir do texto a seguir, poderia identificar e listar quais são os serviços mencionados, o texto é {resultado}'
+    prompt = f"""A partir do texto a seguir, identifique e liste quais são os serviços mencionados, o texto é {resultado}. 
+                 Após, recomende as ferramentas necessárias para todas as tarefas com seu código SAP de acordo com a tabela: 
+                 {formatar_dados_csv("Colinha de Códigos SAP.csv")}, me responda sem usar markdown, apenas como se estivesse 
+                 escrevendo um txt comum."""
+                 
     resposta = gerar_resposta(prompt)
     print(f'Resposta: {resposta}')
 
